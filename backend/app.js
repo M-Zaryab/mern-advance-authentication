2ac5fd49b847475f9f5ab8c3ef6831fd671a2cdf
Zaryab <zaryabimran222@gmail.com>
2024-10-13 12:40:08 +0500

    first commit

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,14 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+// app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin: [""],
+    methods: ["POST", "GET"],
+    credentials: true,
+  })
+);
 
 import authRouter from "./routes/authRoutes.js";
 app.use("/api/v1/auth/", authRouter);
